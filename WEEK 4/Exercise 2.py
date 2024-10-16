ae600c55eb876bdb6bc30bc3f3aa2477799b3a58
Nathan Nnadi <2nnadn68@solent.ac.uk>
2024-10-16 14:48:05 +0100

    mentos

---
+++ WEEK 4/Exercise 2.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, id, name, course, mark)
+        self.id = id
+        self.name = name
+        self.course = course
+        self.mark = mark
+
+student1 = Student(086707, "William", "Software Engineering", 78)
--- WEEK 4/main.py
+++ WEEK 4/main.py
@@ -1,8 +1,16 @@
 from cat import Cat
+cat1 = Cat("Binnie", 4,"British","Black")
+cat1.eat(4)
+cat1.walk(6)
+if cat1.weight >= 1:
+   print(cat1.name, cat1.weight, cat1.breed, cat1.colour)
+else: print("Cat cannot walk");
 
-cat1 = Cat("Binnie", 4)
-cat2 = Cat("Clyde", 2)
-cat1.eat()
-cat2.eat()
-print(cat1.weight)
-print(cat2.weight)
+
+cat3 = Cat("Old Tom", 6, "shortair", "bluish grey")
+cat3.eat(6)
+cat3.walk(5)
+if cat3.weight >= 1:
+   print(cat3.name, cat3.weight, cat3.breed, cat3.colour);
+else:
+    print("Cat cannot walk");
