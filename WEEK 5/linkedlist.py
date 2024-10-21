cbab596f86e592a61e3d67d9a9fa36a8d17a86f6
Nathan Nnadi <2nnadn68@solent.ac.uk>
2024-10-21 11:55:08 +0100

    Halfway through task

--- WEEK 5/linkedlist.py
+++ WEEK 5/linkedlist.py
@@ -1,7 +1,21 @@
 from node import Node
+
 class LinkedList:
     def __init__(self):
+
         self.first = None
         self.last = None
-    def add(self, other):
+
+    def add(self, new_node):
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+            return
+        else:
+            self.last.next = new_node
+
     def get(self, index):
+        pass
+
+    def __str__(self):
+        return self.internalList.__str__()
--- WEEK 5/node_main.py
+++ WEEK 5/node_main.py
@@ -1,6 +1,12 @@
 from node import Node
+from linkedlist import LinkedList
+
 n1 = Node("Fred")
 n2 = Node("Tom")
 
 print(n1)
 print(n2)
+
+node1=LinkedList()
+node1.add(self)
+
