65f875e3dd0e94eebd3cd43f95a144bc0b0d4d7a
IJN-7 <nathannnadi1@gmail.com>
2024-11-12 11:24:16 +0000

    Have now added both the delete method and save method to the program

--- COURSEWORK/exam_ll.py
+++ COURSEWORK/exam_ll.py
@@ -24,18 +24,58 @@ class TupleLinkedLists:
 
 
     def find(self):
-        searchInput = input("Please enter a key to search: ")
-        currentNode = self.head
-        while currentNode is not None:
-            if currentNode.value[0] == searchInput:
-                print(currentNode.value[1])
-                return currentNode.value[1]
+        searchinput = input("Please enter a key to search: ")
+        currentnode = self.head
+        while currentnode is not None:
+            if currentnode.value[0] == searchinput:
+                print(currentnode.value[1])
+                return currentnode.value[1]
             else:
-                currentNode = currentNode.next
+                currentnode = currentnode.next
         return None
+
+    def save(self, file_input):
+        self.file_input = file_input
+        file_input = input("Please enter a POI to save: ")
+        with open('poi.txt', "a") as f:
+            f.write(file_input + '\n')
+        print(f"{file_input} will be appended")
+
+        ans = 0
+        while ans != "y" and ans != "n":
+            ans = input("Would you like to view saved  Points Of Interest? (y/n): ").lower()
+            if ans == 'y':
+                with open('poi.txt', 'r') as f:
+                    for line in f:
+                        for word in line.split():
+                            print(word)
+
+            elif ans == 'n':
+                return None
+
+            else:
+                print("Please enter y or n")
+
+    def dlt(self):
+        del_input = input("Please enter the Point of interest to delete: ")
+        currentnode = self.head
+        while currentnode is not None:
+            if currentnode.value[0] == del_input:
+                print(f" - {currentnode.value[0]} -  will be deleted")
+                currentnode.delete()
+                return
+            else:
+                currentnode = currentnode.next
+        return None
+
+
+
+
 
 tll = TupleLinkedLists()
 tll.add('cat', 'meow')
 tll.find()
+tll.save(file_input='poi.txt')
+tll.dlt()
 
 
--- COURSEWORK/exam_node.py
+++ COURSEWORK/exam_node.py
@@ -4,9 +4,14 @@ class Node:
         self.next = None
         self.value = value
 
-    def link(self, otherNode):
-        self.next = otherNode
-        otherNode.prev = self
+    def link(self, othernode):
+        self.next = othernode
+        othernode.prev = self
+
+    def delete(self):
+        self.prev = None
+        self.next = None
+        self.value = None
 
     def __str__(self):
         return f"Node containing {self.value}"
