4d59f9bebcb74e9a2182f1596910398c3a283b17
IJN-7 <nathannnadi1@gmail.com>
2024-12-11 13:24:27 +0000

    final version

--- COURSEWORK/queue.py
+++ COURSEWORK/queue.py
@@ -1,19 +1,19 @@
 class Queue:
     def __init__(self):
         self.enquiries = []
-
+    # adds queries to the list
     def enqueue(self, query):
         self.enquiries.append(query)
         print(f"Query: *{query}* has been added to the queue.")
         return
-
+    # removes queries in order from the list
     def dequeue(self):
         if len(self.enquiries) == 0:
             return None
         else:
             print(f"Query: *{self.enquiries[0]}* has been removed from the queue.")
             return self.enquiries.pop(0)
-
+    # Displays query list
     def display(self):
 
             if len(self.enquiries) == 0:
